0934dfcaff4d04954f022e2c38e893cf538d0a8a
Dev_Hit <89605089+hit0hit@users.noreply.github.com>
2022-09-22 11:53:52 -0300

    Add files via upload

---
+++ colas/cola1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main () {
+/*repetção são parte do codigo que vai repetir repetidamente 
+ação que se repete uma quantidade de vezes == determinada ==indeterminada==loop infinito
+== 
+
+do
+repita .. whilw 
+para... faça === for (size_t i = 0; i < count; i++)
+
+*/
+return 0;
+}
